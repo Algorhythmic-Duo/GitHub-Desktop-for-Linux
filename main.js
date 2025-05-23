b2a76c30900d93ad0f913eddfdaf1e3dad7c74b7
MathewsVinoy <mathewsvinoy@gmail.com>
2025-05-23 14:35:53 +0530

    hai

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    autoHideMenuBar: true,
+    // autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
